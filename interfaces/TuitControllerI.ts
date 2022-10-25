dba3a9e0bebaedbb6e8c5ece5009a998aeadc12b
radil <adil.r@northeastern.edu>
2022-10-25 11:31:23 -0400

    Adding Interface docstrings

--- interfaces/TuitControllerI.ts
+++ interfaces/TuitControllerI.ts
@@ -2,12 +2,82 @@ import Tuit from "../models/Tuit";
 import {Request, Response} from "express";
 
 export default interface TuitControllerI{
-    //TODO should this return void?
+
+    /**
+     * Gets all users from the database and sends them to the client
+     * @param req {Request} A Request object containing the client's request
+     * as an express.Request object. This is linked to the '/tuits' URI
+     * @param res {Response} A Response object which will be used to send
+     * All Tuits from the database in the format of a JSON array to the client
+     * in its response body
+     */
     findAllTuits(req: Request, res: Response): void;
+
+    /**
+     * Gets a Tuit from the database with an ID matching the ID requested
+     * by a client. It will send the matching Tuit in the response body
+     * in the JSON format.
+     * matching user object in the body fo t
+     * @param req {Request} A Request object containing the client's request
+     * as an express.Request object. The req.params.tid will contain the
+     * user defined tid.
+     * @param res {Response} A Response object that will be used to
+     * send a single Tuit with id matching the userid from the req
+     * to the client
+     */
     findTuitById(req: Request, res: Response): void;
-    // create user will return a new user as a response to let client know it succeded
+
+    /**
+     * Creates a Tuit in the database as defined by the client's req body.
+     * It will also send back the created Tuit to the client as a JSON
+     * in the response body.
+     * @param req {Request} A Request object containing the client's request
+     * as an express.Request object. The req.params.uid must containe
+     * the user id of the user who is "posting" the tweet.
+     * The response body should contain a JSON with the following properties:
+     *      tuit: {string}
+     *      postedOn: {Date} or string rep of a date is ISO format
+     * @param res {Response} A Response object that will be used to
+     * send the created Tuit to the client
+     */
     createTuit(req: Request, res: Response): void;
+
+    /**
+     * Deletes a Tuit from the database whose tuit id matches the client
+     * defined tuit id.
+     * @param req {Request}  A Request object containing the client's request
+     * as an express.Request object. The req.params.tid needs
+     * to contain the client defined tuit id
+     * @param res {Response} A Response object that will send
+     * the amount of deleted tuits to the client
+     */
     deleteTuit(req: Request, res: Response): void;
+
+    /**
+     * Replaces a Tuit from the database with a Tuit
+     * defined by the client as a JSON in the client's
+     * request. It will send the amount of entries updated
+     * back to the user
+     * @param req {Request}  A Request object containing the client's request
+     * as an express.Request object. The req.params.tid needs
+     * to contain the client defined tuit id. Additionally,
+     * the req.body needs to contain the following properties
+     * which will override the properties of the tuit
+     * the client wants to update from the database
+     *      tuit: {string}
+     *      postedOn: {Date} or string rep of a date is ISO format
+     * @param res {Response} A Response object that will send
+     * the amount of updated users to the client
+     */
     updateTuit(req: Request, res: Response): void;
+
+    /**
+     * Gets all Tuits from the database from a user with a userid
+     * defined by the client.
+     * @param req {Request} A Request object containing the client's request
+     * as an express.Request object. The req.params.uid must contain
+     * the client defined user id
+     * @param res
+     */
     findTuitsByUser (req: Request, res: Response) : void;
 }
--- interfaces/UserControllerI.ts
+++ interfaces/UserControllerI.ts
@@ -8,11 +8,85 @@ import User from "../models/User";
  * the express module.
  */
 export default interface UserControllerI {
-
+    /**
+     * Gets all users from the database and sends them to the client
+     * @param req {Request} A Request object containing the client's request
+     * as an express.Request object
+     * @param res {Response} A Response object which will be used to send
+     * All users from the database in the format of a JSON array to the client
+     */
     findAllUsers(req: Request, res: Response): void;
+
+    /**
+     * Gets a user from the database with an ID matching the ID requested
+     * by a client. It will send the matching user in the response body
+     * in the JSON format.
+     * @param req {Request} A Request object containing the client's request
+     * as an express.Request object
+     * @param res {Response} A Response object that will be used to
+     * send a single user with id matching the userid from the req
+     * to the client
+     */
     findUserById(req: Request, res: Response): void;
+
+    /**
+     * Gets a user by their username from the database with a username matching the
+     * username requested by the client. This will send the matching user
+     * in the response body in JSON format.
+     * @param req {Request} A Request object containing the client's request
+     * as an express.Request object
+     * @param res {Response} A Response object that will be used to
+     * send a single user with username matching the username from the req
+     * to the client
+     */
     findUserbyUserName(req: Request, res: Response): void;
+
+    /**
+     * Creates a user in the database as defined by the client's req body.
+     * It will also send back the created user to the client as a JSON
+     * in the response body.
+     * @param req {Request} A Request object containing the client's request
+     * as an express.Request object. The response body should contain
+     * a JSON with the following properties:
+     *      username:
+     *      password:
+     *      firstName:
+     *      lastName:
+     *      email:
+     * @param res {Response} A Response object that will be used to
+     * send the created user to the client
+     */
     createUser(req: Request, res: Response): void;
+
+    /**
+     * Deletes a user from the database whose userid matches the user
+     * defined userid from the client's request.
+     * @param req {Request}  A Request object containing the client's request
+     * as an express.Request object. The req.params['userid'] needs
+     * to contain the client defined userid
+     * @param res {Response} A Response object that will send
+     * the amount of deleted users to the client
+     */
     deleteUser(req: Request, res: Response): void;
+
+    /**
+     * Replaces a user from the database with a user
+     * defined by the client as a JSON in the client's
+     * request. It will send the amount of entries updated
+     * back to the user
+     * @param req {Request}  A Request object containing the client's request
+     * as an express.Request object. The req.params['userid'] needs
+     * to contain the client defined userid. Additionally,
+     * the req.body needs to contain the following properties
+     * which will override the properties of the user
+     * the client wants to update from the database
+     *      username
+     *      password
+     *      firstName
+     *      lastName
+     *      email
+     * @param res {Response} A Response object that will send
+     * the amount of updated users to the client
+     */
     updateUser(req: Request, res: Response): void;
 }
